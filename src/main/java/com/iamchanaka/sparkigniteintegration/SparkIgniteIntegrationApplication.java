243e3f5ef2354bff1bd8c4981183f685dffcc4fc
chanaka <chanakadrathnayaka@gmail.com>
2024-06-19 07:34:39 -0700

    Initial Commit

---
+++ src/main/java/com/iamchanaka/sparkigniteintegration/SparkIgniteIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.iamchanaka.sparkigniteintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkIgniteIntegrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SparkIgniteIntegrationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/iamchanaka/sparkigniteintegration/SparkIgniteIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamchanaka.sparkigniteintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkIgniteIntegrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
